dfe906c9d6eac68663c284ec6046ff992aa494ca
Chiaoan <feel8012@hotmail.com>
2015-02-26 16:37:23 +0800

    poem
    Java

---
+++ test/TEST.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author tcumi_H505
+ */
+public class TEST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("隱隱飛橋隔野煙，石礬西畔問漁船。\n" +"桃花盡日隨流水，洞在清溪何處邊。");
+    }
+}
